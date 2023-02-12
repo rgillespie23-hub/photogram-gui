41f3f4b3e23ad5c416308f10517015b91eeedf1f
rgillespie23 <rgillespie@uchicago.edu>
2023-02-12 04:23:39 +0000

    Done with delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,27 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/index.html.erb"})
   end
+
+  def show
+    #Parameters: {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show.html.erb"})
+  end
+
+  def baii
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
   get("/photos", { :controller => "photos", :action => "index"})
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
 end
